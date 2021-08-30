4b313670a56a23977cc78d62610e0a7b3f912771
Matheus França <mateusanches2@gmail.com>
2021-08-30 20:56:18 -0300

    Rotas do usuário e função de deslogar

---
+++ src/views/usuario/UsuarioEditar.vue
@@ -0,0 +1,10 @@
+<template>
+  <p>editar usuario</p>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ src/views/usuario/UsuarioProdutos.vue
@@ -0,0 +1,12 @@
+<template>
+  <p>Lista de Produtos</p>
+</template>
+
+<script>
+export default {
+  name: 'UsuarioProdutos.vue',
+};
+</script>
+
+<style>
+</style>
