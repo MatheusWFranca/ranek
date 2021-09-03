ffe60667bc134cd49083a131eb8dddb582f1f85c
Matheus França <mateusanches2@gmail.com>
2021-09-02 21:31:29 -0300

    Adicionar Produtos

--- src/components/ProdutoAdicionar.vue
+++ src/components/ProdutoAdicionar.vue
@@ -1,12 +1,65 @@
 <template>
-  <p>Produto Adicionar</p>
+  <form class="adicionar-produto">
+    <label for="nome">Nome</label>
+    <input type="text" id="nome" name="nome" v-model="produto.nome" />
+    <label for="preco">Preço $</label>
+    <input type="number" id="preco" name="preco" v-model="produto.preco" />
+    <label for="fotos">Fotos</label>
+    <input type="file" id="fotos" name="fotos" ref="fotos" />
+    <label for="descricao">Descrição</label>
+    <input
+      type="textarea"
+      id="descricao"
+      name="descricao"
+      v-model="produto.descricao"
+    />
+    <input
+      class="btn"
+      type="button"
+      value="Adicionar Produto"
+      @click.prevent="adicionarProduto"
+    />
+  </form>
 </template>
 
 <script>
+import { api } from '@/services.js';
+
 export default {
   name: 'ProdutoAdicionar',
+  data() {
+    return {
+      produto: {
+        nome: '',
+        preco: '',
+        descricao: '',
+        fotos: null,
+      },
+    };
+  },
+  methods: {
+    formatarProduto() {
+      this.produto.usuario_id = this.$store.state.usuario.id;
+    },
+    adicionarProduto() {
+      this.formatarProduto();
+      api.post('/produto', this.produto).then(() => {
+        this.$store.dispatch('getUsuarioProdutos');
+      });
+    },
+  },
 };
 </script>
 
-<style>
+<style scoped>
+.adicionar-produto {
+  display: grid;
+  grid-template-columns: 100px 1fr;
+  align-items: center;
+  margin-bottom: 60px;
+}
+
+.btn {
+  grid-column: 2;
+}
 </style>
